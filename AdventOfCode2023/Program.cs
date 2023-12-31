42f93d97e8c23a35badfdca1f7f7fa20439a3e70
HalleysAsteroid <95715834+HalleysAsteroid@users.noreply.github.com>
2023-12-31 18:00:22 -0500

    Day1 - Part1

---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+String[] lines;
+
+lines = File.ReadAllLines("../../../../input.txt");
+int total = 0;
+Regex rx = new Regex("[^0-9-]");
+
+foreach (String line in lines)
+{
+    String s = rx.Replace(line, "");
+    String t = "" + s[0] + s[s.Length - 1];
+
+    total += int.Parse(t);
+}
+
+Console.WriteLine(total);
